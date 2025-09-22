24368437fd155dbd682859884c56ab7a49e50308
Sagar Batulwar <batulwarsagar@gmail.com>
2025-09-22 12:56:26 +0530

    user input added

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+            int ageFinal = Convert.ToInt32(age);
+
+            Console.Write("Enter your city: ");
+            string city = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {name} and age is {age} and you are from {city}.");
+        }
+    }
+}
